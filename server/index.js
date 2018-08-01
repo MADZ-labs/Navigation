60beb051b4f21966254439eb9302ae6630483297
amanvaid94@gmail.com <amanvaid94@gmail.com>
2018-07-31 20:25:25 -0700

    Created server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const Log = require('Log');
+
+const app = new express();
+const log = new Log('info');
+const port = process.env.PORT || 3005;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.listen(port, () => {
+  log.info(`Listening on http://localhost:${port}`);
+});
+
